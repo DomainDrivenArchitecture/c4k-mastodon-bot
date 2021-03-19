122a7ddee70057bd75843272bbe935315f7007b7
jem <michael.jerger@meissa-gmbh.de>
2021-03-19 14:16:08 +0100

    first error message

--- src/main/cljs/dda/k8s_mastodon_bot/browser.cljs
+++ src/main/cljs/dda/k8s_mastodon_bot/browser.cljs
@@ -1,12 +1,31 @@
 (ns dda.k8s-mastodon-bot.browser
   (:require
+   [clojure.string :as st]
    [clojure.tools.reader.edn :as edn]
    [expound.alpha :as expound]
    [dda.k8s-mastodon-bot.core :as core]))
 
-(defn config-from-document []
+(defn print-debug [sth]
+  (print "debug " sth)
+  sth)
+
+(defn config []
+    (-> js/document
+      (.getElementById "config")))
+
+(defn set-config-validation! [input]
+  (print-debug input)
   (-> js/document
-      (.getElementById "config")
+      (.getElementById "config-validation")
+      (.-innerHTML)
+      (set! input)))
+
+(defn form []
+  (-> js/document
+      (.getElementById "form")))
+
+(defn config-from-document []
+  (-> (config)
       (.-value)))
 
 (defn auth-from-document []
@@ -14,7 +33,7 @@
       (.getElementById "auth")
       (.-value)))
 
-(defn render-output-to-document
+(defn set-output!
   [input]
   (-> js/document
       (.getElementById "output")
@@ -23,19 +42,34 @@
 
 (defn expound-config
   [config]
-  (expound/expound ::core/config config))
+  (st/replace
+   (expound/expound-str ::core/config config  {:print-specs? false})
+   #"\n" "<br>"))
+
+
+
+(defn render-validation-result-to-config
+  [validation-result]
+  (print-debug validation-result)
+  (set-config-validation! validation-result)
+  (-> (config)
+      (.setCustomValidity validation-result))
+  (-> (form)
+      (.-classList)
+      (.add "was-validated"))
+  validation-result)
 
 (defn init []
   (-> js/document
       (.getElementById "generate-button")
       (.addEventListener "click" 
                          #(-> (core/generate (config-from-document) (auth-from-document))
-                              (render-output-to-document))))
-  (-> js/document
-      (.getElementById "config")
+                              (set-output!))))
+  (-> (config)
       (.addEventListener "blur"
                          #(-> (config-from-document)
                               (edn/read-string)
                               (expound-config)
-                              (print))))
+                              (print-debug)
+                              (render-validation-result-to-config))))
 )
