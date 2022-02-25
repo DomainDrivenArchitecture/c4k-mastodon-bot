1a368a7d50baeeebbf0ea176b62ea21f0a43bb57
jerger <michael.jerger@meissa-gmbh.de>
2022-02-25 10:57:05 +0100

    Version 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/c4k-mastodon-bot "0.1.7-SNAPSHOT"
+(defproject dda/c4k-mastodon-bot "0.1.7"
   :description "mastodon-bot c4k-installation package"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
